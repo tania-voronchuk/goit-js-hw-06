3de71f78f01eb538e3440a309fbe6e76c0bd72be
Tania Voronchuk <tanya.perfekt.pr@gmail.com>
2021-12-29 22:31:39 +0200

    Update task-06.js
    вносить правку в завдання-06

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,10 +3,13 @@ const input = document.getElementById("validation-input");
 console.log(input);
 
 input.addEventListener("blur", onInputChange => {   
-  if(input.value.length == input.dataset.length) { 
-    return input.classList.add("valid") 
-} 
-     input.classList.add("invalid")
-});
+  if(input.value.length + "" === input.dataset.length) { 
+    input.classList.add("valid")
+    input.classList.remove("invalid")
+  }else{ 
+    input.classList.add("invalid")
+    input.classList.remove("valid")
+  }
+  });
 
 
